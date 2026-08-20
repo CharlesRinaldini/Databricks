bc490978e1d6942fbc19541e50725bc3d75d82b5
Charles Rinaldini <rinaldini.charles@gmail.com>
2026-08-20 11:20:50 -0600

    Removed non related content

--- execute-sql/schema/100.gold/100.sqldemo/300.views/300.sqldemo.vw_demo_view.sql
+++
@@ -1,2 +0,0 @@
-CREATE OR REPLACE VIEW {env}_gold.sqldemo.vw_demo_view AS
-SELECT 1 as foo
--- execute-sql/schema/100.gold/100.sqldemo/900.scripts/900.gold.sqldemo.update_demo_table.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO {env}_gold.sqldemo.demo_table (string_key, test_string, modified_date, is_active)
-VALUES ('Z', 'new demo value', CURRENT_TIMESTAMP(), TRUE);
